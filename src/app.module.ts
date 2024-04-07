ddbcb8b82320ad59a7eb636e3203fb8d5f8f902e
programmerkhadka <rituparnak@claritytech.io>
2024-04-07 11:06:13 +0530

    chore: Some setup work

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: 'password',
-      database: 'headless-cms',
+      port: parseInt(process.env.FORWARD_POSTGRES_PORT, 10) || 5432,
+      username: process.env.POSTGRES_USER || 'postgres', // Default user postgress to access server
+      password: process.env.POSTGRES_PASSWORD || 'password', // Password that is set in docker-compose
+      database: process.env.POSTGRES_DB || 'postgres', // Default db at very beganing we are accessing. After conncetion established manualy create db and put here
       autoLoadEntities: true,
       synchronize: true, // For production make sure disable it
     }),
